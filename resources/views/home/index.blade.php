85890b6ccbbaa727c4757e4d7b3b59308c1e118b
Rutger Vos <99033234@mydavinci.nl>
2021-12-01 11:13:47 +0100

    added more info about me

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,24 +1,34 @@
 <x-app-layout>
-    <x-slot name="header">
-    </x-slot>
-<body>
+  <head>
+  @livewireStyles
+  @livewireScripts
+  </head>
  <div class="text-center">
   <img src="https://media-exp1.licdn.com/dms/image/C4E03AQEofUl2Z8O_oQ/profile-displayphoto-shrink_200_200/0/1637412866287?e=1643241600&v=beta&t=wNq-b2EOkhyw2K6dP3-U6HHdTBXLBmb9gYiAiBJt70U" 
             class="img-fluid" alt="Responsive image" >
     <p class="font-weight-bolder">Rutger Vos</p>
-     <a class="btn btn-primary" href="https://www.linkedin.com/in/rutger-vos-5631b9207/" role="button">Linkedin</a>
+     <a class="btn btn-primary" href="https://www.linkedin.com/in/rutger-vos-5631b9207/" role="button" target="_blank">Linkedin</a>
+     <a class="btn btn-info" href="mailto:rutger564@gmail.com" role="button">E-mail</a>
+     <address>
+    Acaciastraat 16, Dordrecht<br>
+    Mobiel: 06-11482684<br>
+    NL
+  </address>
  </div>
-            <p class="font-weight-bolder">HTML</p>
-            <div class="progress">
+    <p class="font-weight-bolder">HTML</p>
+  <div class="progress">
   <div class="progress-bar bg-success" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">redelijke goed 75%</div>
 </div>
 <p class="font-weight-bolder">CSS</p>
 <div class="progress">
-  <div class="progress-bar bg-danger" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">redelijke goed 75%</div>
+  <div class="progress-bar bg-primary" role="progressbar" style="width: 60%" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">redelijke goed 60%</div>
 </div>
 <p class="font-weight-bolder">PYTHON</p>
 <div class="progress">
   <div class="progress-bar bg-info" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">basis 25%</div>
 </div>
-</body>
+<p class="font-weight-bolder">PHP</p>
+<div class="progress">
+  <div class="progress-bar bg-primary" role="progressbar" style="width: 85%" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">beste goed 85%</div>
+</div>
 </x-app-layout>
