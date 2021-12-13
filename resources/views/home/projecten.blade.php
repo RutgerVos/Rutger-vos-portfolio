c8385b19dfd88bd8ffaef552705e82b2f79538f2
Rutger Vos <99033234@mydavinci.nl>
2021-12-13 15:11:56 +0100

    made some changes

--- resources/views/home/projecten.blade.php
+++ resources/views/home/projecten.blade.php
@@ -4,8 +4,8 @@
   @livewireScripts
   </head>
   <div class="text-center">
-	  <h1 href="">projecten waar ik aan gewerkt heb</h1>
-	  <h2>Studenten-Portaal</h2>
+	  <h1 href="">projecten waar ik aan gewerkt heb.</h1>
+	  <h2>Studenten-Portaal:</h2>
     <span>Is een project maar ik aan gewerkt heb voor een leeraar om studenten makkelijke
       vragen te kunnen laten stellen en leraren makkelijke vragen te  kunnen  laten beantwoorden.
 
@@ -14,7 +14,7 @@
     <a class="font-weight-bold" href="https://github.com/RutgerVos/studenten-portaal" alt="http://studenten-portaal.herokuapp.com" target="_blank">Studenten-Portaal GIT</a>
 	  <!-- <p class="font-italic">gebruikersnaam:</p>
 	  <p class="font-italic">wachtwoord:</p> -->
-	  <h3>shopping-cart</h3>
+	  <h3>shopping-cart:</h3>
 	  <span>
 		  Is een project die gemaakt heb in mijn school tijd als intro voor 
 		  database relationships met laravel.
@@ -24,11 +24,11 @@
 	  </span>
 	  <p>Als je wilt kan je het hier downloaden</p>
 	  <a class="font-weight-bold" href="https://github.com/RutgerVos/RutgerVos-MPA---Webshop-Shopping-Cart" target="_blank">shopping-cart GIT</a>
-	  <h4>to do list</h4>
+	  <h4>to do list:</h4>
 	  <span>
 		  Is een project dat ik heb gemaakt om lijst te maken van dingen die je nog moet doen.
 	  </span>
 	  <p>Als je wilt kan je het hier downloaden</p>
-	  <a class="font-weight-bold" href="https://github.com/RutgerVos/to-do-list.git" target="_blank"> GIT</a>
+	  <a class="font-weight-bold" href="https://github.com/RutgerVos/to-do-list.git" target="_blank">To-Do-list GIT</a>
   </div>
 </x-app-layout>
